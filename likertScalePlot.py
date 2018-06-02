7f8bcdf94e7cfffaaae78ff320751f1eeb520c10
Olivier Philippe <olivier.philippe@gmail.com>
2018-06-02 16:42:44 +0200

    some small code cleaning

--- likertScalePlot.py
+++ likertScalePlot.py
@@ -17,22 +17,10 @@ __licence__ = 'BSD 3-clause'
 import math
 import pandas as pd
 import numpy as np
-import matplotlib
-
-# When using Ipython within vim
-matplotlib.use('TkAgg')
-
-# When using within jupyter
-# get_ipython().magic('matplotlib inline')  # Activat that line to use in Jupyter
-
-
 import matplotlib.pyplot as plt
-#  When using this script with ipython and vim
-plt.ion()
-plt.show()
 
 
-def get_colors(df, colormap=plt.cm.RdBu, vmin=None, vmax=None):
+def get_colors(df, colormap=plt.cm.RdBu, vmin=None, vmax=None, axis=1):
     """
     Function to automatically gets a colormap for all the values passed in,
     Have the option to normalise the colormap.
@@ -48,12 +36,41 @@ def get_colors(df, colormap=plt.cm.RdBu, vmin=None, vmax=None):
 
     Original version found on stackerOverflow (w/o the try/except) but cannot find it back
     """
-    values = df.columns
+    if axis == 0:
+        values = df.index
+    elif axis == 1:
+        values = df.columns
     norm = plt.Normalize(vmin, vmax)
     try:
         return colormap(norm(values))
     except (AttributeError, TypeError):  # May happen when gives a list of categorical values
         return colormap(norm(range(len(values))))
+
+
+def wrap_labels(label, max_size=30):
+    """
+    Function to automatically wrap labels if they are too long
+    Split only if whitespace
+    params:
+        :labels str(): string that contains the labels
+        :max_size int(): 20 by Default, the size of the string
+        before being wrapped
+    :return:
+        :str() of wrapped labels according to the max size
+    """
+    def split_at_whitespace(label):
+        label_to_return = list()
+        n = 0
+        for letter in label:
+            n +=1
+            if n >= max_size:
+                if letter == ' ':
+                    letter = '\n'
+                    n = 0
+            label_to_return.append(letter)
+        return ''.join(label_to_return)
+
+    return split_at_whitespace(label)
 
 
 def create_bars(df, ax, y_pos, colors, left_gap):
@@ -135,20 +152,20 @@ def compute_percentage(df, by_row=True, by_col=False):
     """
     Transform every cell into a percentage
     """
-    def compute_percentage(row, total=None):
+    def compute_perc(row, total=None):
         if total is None:
             total = np.sum(row)
-        return [np.round(((x /total) *100), 2) for x in row]
+        return [((x /total) *100) for x in row]
 
     if by_row is True and by_col is False:
-        return np.array(df.apply(compute_percentage, axis=1))
+        return np.array(df.apply(compute_perc, axis=1))
 
     elif by_col is True and by_row is False:
-        return np.array(df.apply(compute_percentage, axis=0))
+        return np.array(df.apply(compute_perc, axis=0))
 
     elif by_row is True and by_col is True:
         total = df.values.sum()
-        return np.array(df.apply(compute_percentage, total=total))
+        return np.array(df.apply(compute_perc, total=total))
 
 
 def normalise_per_row(df):
@@ -156,7 +173,7 @@ def normalise_per_row(df):
     return df.multiply(100)
 
 
-def add_labels(df, ax, bars, rotation=0):
+def add_labels(df, ax, bars, rotation=0, rounding=True):
     """
     """
     # Create percentage for each cells to have the right annotation
@@ -167,7 +184,12 @@ def add_labels(df, ax, bars, rotation=0):
             bl = bar.get_xy()
             x = 0.5 *bar.get_width() +bl[0]
             y = 0.5 *bar.get_height() +bl[1]
-            ax.text(x, y, "{}".format(percentages[i, j]), ha='center', rotation=rotation)
+            # Avoid labels when percentage is under 5 (the bar is too small)
+            if percentages[i, j] > 5:
+                if rounding is True:
+                    ax.text(x, y, "{}".format(str(int(round(percentages[i, j])))), ha='center', rotation=rotation)
+                else:
+                    ax.text(x, y, "{}".format(percentages[i, j]), ha='center', rotation=rotation)
 
 
 def draw_middle_line(normalise, longest_middle):
@@ -190,7 +212,6 @@ def drawing_x_labels(normalise, complete_longest, longest_middle):
         xvalues = range(0, 210, 10)
         xlabels = [str(math.floor(abs(x - 100))) for x in xvalues]
     else:
-        print('NOT normalised')
         xvalues = [math.floor(i - (longest_middle %5))
                    for i in range(0, int(complete_longest),
                                   int(int(longest_middle)/ 5))]
@@ -198,106 +219,97 @@ def drawing_x_labels(normalise, complete_longest, longest_middle):
     plt.xticks(xvalues, xlabels)
 
 
-def likert_scale(df, normalise=True, labels=True, middle_line=True, legend=True, rotation=0):
+def likert_scale(df, normalise=True, labels=True, middle_line=True, legend=True, rotation=0, title_plot=False, rounding=True):
     """
     The idea is to create a fake bar on the left to center the bar on the same point.
     :params:
     :return:
     """
-    # Create the figure object
-    fig = plt.figure(figsize=(10, 8))
-    # Create an axes object in the figure
-    ax = fig.add_subplot(111)
+    # Replace the Nan value by 0 for plotting
+    df = df.fillna(0)
+    try:
+        # Create the figure object
+        # if figsize is None:
+        #     fig = plt.figure(figsize=(10, 8))
+        # else:
+        #     fig = plt.figure(figsize=figsize)
+        # # Create an axes object in the figure
+        fig, ax = plt.subplots()
 
-    # Generate an array of colors based on different colormap. The default value
-    # Use a divergent colormap.
-    colors = get_colors(df)
+        # Generate an array of colors based on different colormap. The default value
+        # Use a divergent colormap.
+        colors = get_colors(df)
 
-    # Get the position of each bar for all the items
-    y_pos = np.arange(len(df.index))
+        # Get the position of each bar for all the items
+        y_pos = np.arange(len(df.index))
 
-    if normalise:
-        df = normalise_per_row(df)
+        if normalise:
+            df = normalise_per_row(df)
 
-    # Compute the middle of the possible answers. Assuming the answers are columns
-    # Get the sum of the middles +.5 if middle value and without .5 if splitted in 2
-    # equal divides
-    middles = get_total_mid_answers(df)
+        # Compute the middle of the possible answers. Assuming the answers are columns
+        # Get the sum of the middles +.5 if middle value and without .5 if splitted in 2
+        # equal divides
+        middles = get_total_mid_answers(df)
 
-    # Calculate the longest middle bar to set up the middle of the x-axis for the x-lables
-    # and plot the middle line
-    if normalise:
-        longest_middle = 100
-    else:
-        longest_middle = middles.max()
-    print('LONGEST MIDDLE: {}'.format(longest_middle))
+        # Calculate the longest middle bar to set up the middle of the x-axis for the x-lables
+        # and plot the middle line
+        if normalise:
+            longest_middle = 100
+        else:
+            longest_middle = middles.max()
 
-    # Create the left bar to centre the barchart in the middle
-    left_invisible_bar = np.array((middles - longest_middle).abs())
+        # Create the left bar to centre the barchart in the middle
+        left_invisible_bar = np.array((middles - longest_middle).abs())
 
-    # Calculate the longest bar with the left gap in it to plot the x_value at the end
-    # Calculate the total of the longest bar to have the appropriate width +
-    # the invisible bar in case it is used to center everything
-    complete_longest = (df.sum(axis=1) + left_invisible_bar).max()
+        # Calculate the longest bar with the left gap in it to plot the x_value at the end
+        # Calculate the total of the longest bar to have the appropriate width +
+        # the invisible bar in case it is used to center everything
+        complete_longest = (df.sum(axis=1) + left_invisible_bar).max()
 
-    # Create the horizontal bars
-    bars = create_bars(df, ax, y_pos, colors, left_invisible_bar)
+        # Create the horizontal bars
+        bars = create_bars(df, ax, y_pos, colors, left_invisible_bar)
 
-    # Set up the limit from 0 to the longest total barchart
-    # Keeping this drawing before drawing_x_labels or it will failed to draw
-    # all the labels on the right side
-    ax.set_xlim([-0.5, complete_longest + 0.5])
+        # Set up the limit from 0 to the longest total barchart
+        # Keeping this drawing before drawing_x_labels or it will failed to draw
+        # all the labels on the right side
+        ax.set_xlim([-0.5, complete_longest + 0.5])
 
-    # Drawing x_labels
-    drawing_x_labels(normalise, complete_longest, longest_middle)
-    ax.set_xlabel('Percentages')
+        # Drawing x_labels
+        drawing_x_labels(normalise, complete_longest, longest_middle)
+        ax.set_xlabel('Percentage')
 
-    # Setting up the y-axis
-    ax.set_yticks(y_pos)
-    ax.set_yticklabels(df.index)
+        # Setting up the y-axis
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels([wrap_labels(labels) for labels in df.index], fontsize=14)
 
-    # Add labels to each box
-    if labels:
-        add_labels(df, ax, bars, rotation)
+        # Add labels to each box
+        if labels:
+            add_labels(df, ax, bars, rotation, rounding=rounding)
 
-    # Create a line on the middle
-    if middle_line:
-        draw_middle_line(normalise, longest_middle)
+        # Create a line on the middle
+        if middle_line:
+            draw_middle_line(normalise, longest_middle)
 
-    # Add legend
-    if legend:
-        ax.legend(bars, df.columns)
+        # Add legend
+        if legend:
+            ax.legend(bars, df.columns, fontsize=14)
 
-
-def count_unique_value(df, colnames, rename_columns=False, dropna=False, normalize=False):
-    """
-    Count the values of different columns and transpose the count
-    :params:
-        :df pd.df(): dataframe containing the data
-        :colnames list(): list of strings corresponding to the column header to select the right column
-    :return:
-        :result_df pd.df(): dataframe with the count of each answer for each columns
-    """
-    # Subset the columns
-    df_sub = df[colnames]
-
-    if rename_columns is True:
-        df_sub.columns = [s.split('[', 1)[1].split(']')[0] for s in colnames]
-
-    # Calculate the counts for them
-    df_sub = df_sub.apply(pd.Series.value_counts, dropna=dropna, normalize=normalize)
-    # Transpose the column to row to be able to plot a stacked bar chart
-    return df_sub.transpose()
+        # Change the plot title
+        if title_plot:
+            plt.suptitle(title_plot)
+        return ax
+    except Exception:
+        raise
 
 
 def main():
     """
     """
 
-    # df = pd.DataFrame(np.random.randint(0,100,size=(100, 3)), columns=list('XYZ'))
     dummy = pd.DataFrame([[1, 2, 3, 4, 5, 2], [5, 6, 7, 8, 5, 2], [10, 4, 2, 10, 5, 2]],
                          columns=["SD", "D", "N", "A", "SA", 'TEST'],
                          index=["Key 1", "Key B", "Key III"])
+
     likert_scale(dummy)
 
 
